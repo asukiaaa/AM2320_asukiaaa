dbe14e7fdeea509d7a2a8958f8fa0798db4c7976
Asuk Kono <asukiaaa@gmail.com>
2019-06-29 15:48:59 +0900

    Define values without using enum

--- src/AM2320_asukiaaa.cpp
+++ src/AM2320_asukiaaa.cpp
@@ -22,35 +22,34 @@ void AM2320_asukiaaa::setWire(TwoWire* wire) {
   myWire = wire;
 }
 
-AM2320_asukiaaa::Errors AM2320_asukiaaa::update() {
+int AM2320_asukiaaa::update() {
   static const int buffLen = 8;
   byte buf[buffLen];
-  Errors result;
 
   // Wakeup sensor
-  myWire->beginTransmission(AM2320_ADDRESS);
+  myWire->beginTransmission(AM2320_asukiaaa_DEVICE_ADDRESS);
   myWire->endTransmission(); // Don't use value of endTransmission on this time because it returns error when the sensor slept.
   delay(10);
 
-  myWire->beginTransmission(AM2320_ADDRESS);
+  myWire->beginTransmission(AM2320_asukiaaa_DEVICE_ADDRESS);
   myWire->write((unsigned char) 0x03);
   myWire->write((unsigned char) 0x00);
   myWire->write((unsigned char) 0x04);
-  result = (Errors) myWire->endTransmission(true);
-  if (result != Errors::SUCCESS) return result;
+  int result = myWire->endTransmission(true);
+  if (result != AM2320_asukiaaa_SUCCESS) return result;
   delay(2); // >1.5ms
-  myWire->requestFrom(AM2320_ADDRESS, buffLen);
+  myWire->requestFrom(AM2320_asukiaaa_DEVICE_ADDRESS, buffLen);
   for (uint8_t i = 0; i < buffLen; ++i) {
     buf[i] = myWire->read();
   }
 
   // Fusion Code check
-  if (buf[0] != 0x03) return Errors::FUSION_CODE_UNMATCH;
+  if (buf[0] != 0x03) return AM2320_asukiaaa_ERROR_FUSION_CODE_UNMATCH;
 
   // CRC check
   uint16_t Rcrc = buf[7] << 8;
   Rcrc += buf[6];
-  if (Rcrc != CRC16(buf, 6)) return Errors::CRC_UNMATCH;
+  if (Rcrc != CRC16(buf, 6)) return AM2320_asukiaaa_ERROR_CRC_UNMATCH;
 
   uint16_t t = (((uint16_t) buf[4] & 0x7F) << 8) | buf[5];
   temperatureC = t / 10.0;
@@ -60,5 +59,5 @@ AM2320_asukiaaa::Errors AM2320_asukiaaa::update() {
   uint16_t h = ((uint16_t) buf[2] << 8) | buf[3];
   humidity = h / 10.0;
 
-  return Errors::SUCCESS;
+  return AM2320_asukiaaa_SUCCESS;
 }
--- src/AM2320_asukiaaa.h
+++ src/AM2320_asukiaaa.h
@@ -1,26 +1,26 @@
 #ifndef AM2320_ASUKIAAA_H
 #define AM2320_ASUKIAAA_H
 
+#define AM2320_asukiaaa_DEVICE_ADDRESS 0x5C // (0xB8 >> 1)
+
+#define AM2320_asukiaaa_SUCCESS                             0
+#define AM2320_asukiaaa_ERROR_WIRE_DATA_TOO_LONG            1
+#define AM2320_asukiaaa_ERROR_WIRE_RECEIVED_NACK_OF_ADDRESS 2
+#define AM2320_asukiaaa_ERROR_WIRE_RECEIVED_NACK_OF_DATA    3
+#define AM2320_asukiaaa_ERROR_WIRE_OTHER_ERROR              4
+#define AM2320_asukiaaa_ERROR_FUSION_CODE_UNMATCH           5
+#define AM2320_asukiaaa_ERROR_CRC_UNMATCH                   6
+
 #include <Arduino.h>
 #include <Wire.h>
 
-#define AM2320_ADDRESS 0x5C // (0xB8 >> 1)
+#define Errors 1
 
 class AM2320_asukiaaa {
   public:
-  enum Errors {
-    SUCCESS = 0,
-    WIRE_DATA_TOO_LONG = 1,
-    WIRE_RECEIVED_NACK_OF_ADDRESS = 2,
-    WIRE_RECEIVED_NACK_OF_DATA = 3,
-    WIRE_OTHER_ERROR = 4,
-    FUSION_CODE_UNMATCH = 5,
-    CRC_UNMATCH = 6,
-  };
-
   AM2320_asukiaaa();
   void setWire(TwoWire *wire);
-  Errors update();
+  int update();
   float temperatureC;
   float temperatureF;
   float humidity;
